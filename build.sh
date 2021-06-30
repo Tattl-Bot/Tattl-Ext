e9fdfaa14d5e209e8c943ca933d1b50bc36be051
AvanRocks <64660241+AvanRocks@users.noreply.github.com>
2021-06-30 17:57:43 -0400

    version name in file name

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-zip tattl -r -FS ../tattl-ext --exclude *.git* ../tattl-ext/resources ../tattl-ext/build.sh
+zip tattl-0.1.0 -r -FS ../tattl-ext --exclude *.git* ../tattl-ext/resources ../tattl-ext/build.sh
